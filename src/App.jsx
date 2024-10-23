496627d26265d7e6c1ac7e77382e1c7ef55d69e6
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-23 12:13:22 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Banner from "./components/Banner"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Banner></Banner>
+      
+     
+    </>
+  )
+}
+
+export default App
